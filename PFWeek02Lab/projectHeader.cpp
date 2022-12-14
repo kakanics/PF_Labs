98a70c568fc62b252cd27d683b68aeac7fe98968
affan ahmed <affan.bb7@gmail.com>
2022-12-14 20:41:03 +0500

    Lab 2, all 6 tasks

---
+++ PFWeek02Lab/projectHeader.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+cout << "**************************************************************"<<endl;
+cout << "*                                                            *"<<endl;
+cout << "*                  Azeem medical Store                       *"<<endl;
+cout << "*              Inventory management system                   *"<<endl;
+cout << "*                                                            *"<<endl;
+cout << "*                                                            *"<<endl;
+cout << "**************************************************************"<<endl;
+
+}
